5791d356eeb006ed2e6b22682b093e7e0696c257
tanveermurad <tanveermuradbalch@gmail.com>
2025-12-18 02:56:07 +0500

    ain

--- chapter 9 to 11/app.js
+++ chapter 9 to 11/app.js
@@ -100,11 +100,117 @@ if (true){
 if (false){
     alert("False");
 }
+// it is working but i am not aware why
 if("car" < "cat"){
     alert("car is smaller than cat");
 }
 
+// question 6
+var subject1Mark = Number(prompt("Enter marks obtained in subject 1"));
+var subject2Mark = Number(prompt("Enter marks obtained in subject 1"));
+var subject3Mark = Number(prompt("Enter marks obtained in subject 1"));
+var totalMarks = 300;
+var obtainedMarks = subject1Mark + subject2Mark + subject3Mark;
+var percentage = (obtainedMarks / totalMarks) * 100;
+
+document.write("<h1> Marks Sheet </h1> <br> <br>");
+document.write("Total Marks: " + totalMarks + "<br>");
+document.write("Marks Obtained: " + obtainedMarks + "<br>");
+document.write("Percentage: " + percentage + "% <br>");
+
+if (percentage >= 80){
+    document.write("Grade: A-one <br>");
+    document.write("Remarks: Excellent <br>");
+}
+
+else if (percentage >= 70){
+    document.write("Grade: A <br>");
+    document.write("Remarks: Good <br>");
+}
+else if (percentage >= 60){
+    document.write("Grade: B <br>");
+    document.write("Remarks: You need to improve <br>");
+}
+else {
+    document.write("Grade: Fail <br>");
+    document.write("Remarks: Sorry <br>");
+}
+
+// Question 7
+
+var secretNum = 7;
+var userGuess = Number(prompt("Guess the secret number (between 1 and 10)"));
+if ( userGuess === secretNum){
+    alert("Bingo! Correct answer");
+}
+if (userGuess === secretNum + 1 || userGuess === secretNum - 1){
+    alert("Close enough to the correct answer");
+}
+// Question 8
+
+var num = Number(prompt("Enter a number to check if it is divisible by 3"));
+if (num % 3 === 0){
+    alert(num + " is divisible by 3");
+}
+else {
+    alert(num + " is not divisible by 3");
+}
+
+
+// Question 9
+var num = Number(prompt("Enter a number to check if it is even or odd"));
+if (num % 2 === 0){
+    alert(num + " is even");
+}
+else {
+    alert(num + " is odd");
+};
 
 
 
+// question 10
+
+var temp = prompt("enter temperature");
+
+if(temp>"40"){
+    alert("it is too hot outside")
+}
+else if(temp > "30"){
+    "whether is normal"
+}
+else if(temp >"20"){
+    alert("today wheather is cool")
+}
+else if(temp> "10"){
+    alert("OMG! whether is very cool")
+
+}
+// Question 11
+
+var firstNum = number=(prompt("Enter first number to calculate "));
+var SecondNum = number=(prompt("Enter second number to calculate "));
+var operater = prompt(" enter operater");
+
+
+if(operater=="+"){
+    alert("THE SUM  IS " + firstNum+SecondNum)
+}
+
+ else if(operater=="-"){
+    alert("THE SUbs  IS " + firstNum-SecondNum)
+}
+
+ else if(operater=="/"){
+    alert("THE RESULT IS " + firstNum/SecondNum)
+}
+
+
+ else if(operater=="*"){
+    alert("THE RESULT IS " + firstNum*SecondNum)
+}
+
+
+else if(operater=="%"){
+    alert("THE RESULT IS " + firstNum%SecondNum)
+}
 
